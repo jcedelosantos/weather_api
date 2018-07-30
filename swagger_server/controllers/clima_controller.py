aedf340e5eb4af17dea9b556727cc14112f49bb7
javiscta <javis.cta@gmail.com>
2018-07-30 14:16:49 -0400

    Soporte básico para las dos operaciones definidas en el API

--- swagger_server/controllers/clima_controller.py
+++ swagger_server/controllers/clima_controller.py
@@ -1,8 +1,15 @@
 import connexion
 import six
 
+# Objeto que permite operar con la tabla de la BD
+from swagger_server import db
 from swagger_server.models.medicion import Medicion  # noqa: E501
+# Tabla de SQLAlchemy de las mediciones
+from swagger_server.sqlalchemy_models import Medicion as Med
 from swagger_server import util
+# Registro del log del sistema
+import logging
+logging.basicConfig(format='%(levelname)s:[%(filename)s:%(lineno)s - %(funcName)20s() ]:%(message)s', level=logging.INFO)
 
 
 def clima_id_estacion_get(idEstacion, inicio=None, fin=None, sensor=None):  # noqa: E501
@@ -23,4 +30,18 @@ def clima_id_estacion_get(idEstacion, inicio=None, fin=None, sensor=None):  # no
     """
     inicio = util.deserialize_datetime(inicio)
     fin = util.deserialize_datetime(fin)
-    return 'do some magic!'
+
+    # Obtenemos el listado de los registros de la estación consultada
+    consulta = Med.query.filter_by(hardware=idEstacion).all()
+
+    # Variable de salida
+    res = list()
+    # Recorremos los resultados de la consulta.
+    for fila in consulta:
+        # Llenamos el JSON basado en la data de la fila 
+        f = fila.__dict__
+        f.pop('_sa_instance_state')
+        # Agregamos la medición al vector de salida
+        res.append(f)
+
+    return res, 200
--- swagger_server/controllers/estacion_controller.py
+++ swagger_server/controllers/estacion_controller.py
@@ -5,8 +5,8 @@ import six
 from swagger_server.models.medicion import Medicion  # noqa: E501
 from swagger_server import util
 # Objeto que permite operar con la tabla de la BD
-from swagger_server.sqlalchemy_models import Medicion as Med
 from swagger_server import db
+from swagger_server.sqlalchemy_models import Medicion as Med
 # Registro del log del sistema
 import logging
 logging.basicConfig(format='%(levelname)s:[%(filename)s:%(lineno)s - %(funcName)20s() ]:%(message)s', level=logging.INFO)
@@ -21,21 +21,19 @@ def estacion_post(body):  # noqa: E501
 
     :rtype: None
     """
-    if connexion.request.is_json:
-        body = Medicion.from_dict(connexion.request.get_json())  # noqa: E501
-    # Transformamos el objeto Medicion, hacia un diccionario de python.
-    data_dict = body.to_dict()
-    data_dict['idHardware'] = data_dict['id_hardware']
-    data_dict['presionBar'] = data_dict['presion_bar']
-    data_dict.pop('id_hardware')
-    data_dict.pop('presion_bar')
-    # logging.info(data_dict)
-    # return "DEBUG", 200
-    
-    # 
-    data_sensor = Med(**data_dict)
-    db.session.add(data_sensor)
-    db.session.commit()
+    try:
+        if connexion.request.is_json:
+            body = Medicion.from_dict(connexion.request.get_json())  # noqa: E501
+        # Transformamos el objeto Medicion, hacia un diccionario de python.
+        data_dict = body.to_dict()
+        data_dict.pop('id')
+        # logging.info(data_dict)
+        # return "DEBUG", 200
 
+        data_sensor = Med(**data_dict)
+        db.session.add(data_sensor)
+        db.session.commit()
+    except Exception as e:
+        return "Error inesperado. Detalles [{0}]".format(e), 500
 
-    return "DEBUG", 200
+    return "Registro satisfactorio.", 200
